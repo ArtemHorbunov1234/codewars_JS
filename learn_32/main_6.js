85c80dd7ea741dab0886fd2d7fef9c4a2d049305
Artem <kolyasik124@ukr.net>
2023-12-04 21:29:43 +0200

    task

--- learn_32/main_6.js
+++ learn_32/main_6.js
@@ -1,23 +1,93 @@
 function dirReduc(arr) {
-    let leftRight = 0;
-    let topDown = 0;
-    let array = [];
+    let count = {};
+    let countPath = {};
+    const array = [];
+
     let nWSE = ['NORTH', 'WEST', 'SOUTH', 'EAST'];
     if (arr[0] === 'NORTH' && arr[1] === 'WEST' && arr[2] === 'SOUTH' && arr[3] === 'EAST') return nWSE;
-    for (let a = 0; a < arr.length; a++) {
-        const count = arr[a];
-        if (count === 'WEST') leftRight++;
-        else if (count === 'EAST') leftRight--;
-        else if (count === 'NORTH') topDown++;
-        else if (count === 'SOUTH') topDown--;
-    }
-    if (topDown > 0) array.push('NORTH');
-    if (leftRight > 0) array.push('WEST');
-    if (topDown < 0) array.push('SOUTH');
 
-    if (leftRight < 0) array.push('EAST');
+    for (let a = 0; a < arr.length; a++) {
+        const direction = arr[a];
+        count[direction] = (count[direction] || 0) + 1;
+    }
+
+    countPath.EAST = Math.max(count.EAST - (count.WEST || 0), 0);
+    countPath.WEST = Math.max(count.WEST - (count.EAST || 0), 0);
+    countPath.NORTH = Math.max(count.NORTH - (count.SOUTH || 0), 0);
+    countPath.SOUTH = Math.max(count.SOUTH - (count.NORTH || 0), 0);
+
+    for (const direction in countPath) {
+        if (countPath[direction] > 0) {
+            for (let i = 0; i < countPath[direction]; i++) {
+                array.push(direction);
+            }
+        }
+    }
 
     return array;
 }
 
-console.log(dirReduc(['EAST', 'EAST', 'WEST', 'NORTH', 'WEST', 'EAST', 'EAST', 'SOUTH', 'NORTH', 'WEST']));
+console.log(
+    dirReduc([
+        'SOUTH',
+        'NORTH',
+        'EAST',
+        'WEST',
+        'NORTH',
+        'SOUTH',
+        'NORTH',
+        'NORTH',
+        'NORTH',
+        'WEST',
+        'EAST',
+        'NORTH',
+        'SOUTH',
+        'WEST',
+        'EAST',
+    ])
+);
+
+function dirReduc(arr) {
+    const oppositeDirections = {
+        NORTH: 'SOUTH',
+        SOUTH: 'NORTH',
+        EAST: 'WEST',
+        WEST: 'EAST',
+    };
+
+    const stack = [];
+
+    for (const direction of arr) {
+        const lastDirection = stack.pop();
+
+        if (lastDirection && lastDirection === oppositeDirections[direction]) {
+            // Противоположные направления - удаляем из стека
+            continue;
+        }
+
+        // Не противоположные направления - добавляем в стек
+        stack.push(lastDirection, direction);
+    }
+
+    return stack.filter(Boolean); // Фильтруем, чтобы убрать null (undefined) значения
+}
+
+console.log(
+    dirReduc([
+        'SOUTH',
+        'NORTH',
+        'EAST',
+        'WEST',
+        'NORTH',
+        'SOUTH',
+        'NORTH',
+        'NORTH',
+        'NORTH',
+        'WEST',
+        'EAST',
+        'NORTH',
+        'SOUTH',
+        'WEST',
+        'EAST',
+    ])
+);
